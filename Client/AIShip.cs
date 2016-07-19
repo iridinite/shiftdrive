3b998166e25a031fdd625f0895e743d91ff48191
iridinite <codingcuddlewolf@gmail.com>
2016-07-20 00:04:29 +0200

    Split PlayerShip and AIShip to seperate files
    If and when the classes are going to get bigger, especially the AI for
    NPC ships, separating the classes into their own files should make
    Ship.cs much less messy.

---
+++ Client/AIShip.cs
@@ -0,0 +1,19 @@
+﻿/*
+** Project ShiftDrive
+** (C) Mika Molenkamp, 2016.
+*/
+
+namespace ShiftDrive {
+
+    /// <summary>
+    /// Represents an AI-controlled ship.
+    /// </summary>
+    internal sealed class AIShip : Ship {
+
+        public override void Update(GameState world, float deltaTime) {
+            base.Update(world, deltaTime);
+        }
+
+    }
+
+}
