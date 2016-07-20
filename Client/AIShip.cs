4a385dee81def307d340f14f5bb0a0249e509c77
iridinite <codingcuddlewolf@gmail.com>
2016-07-20 19:38:16 +0200

    Add AIShip constructor
    The GameObject.type field must be set for the client to properly
    recognize and deserialize the object.

--- Client/AIShip.cs
+++ Client/AIShip.cs
@@ -3,12 +3,21 @@
 ** (C) Mika Molenkamp, 2016.
 */
 
+using Microsoft.Xna.Framework;
+
 namespace ShiftDrive {
 
     /// <summary>
     /// Represents an AI-controlled ship.
     /// </summary>
     internal sealed class AIShip : Ship {
+
+        public AIShip() {
+            type = ObjectType.AIShip;
+            iconfile = "player";
+            iconcolor = Color.Blue;
+            bounding = 10f;
+        }
 
         public override void Update(GameState world, float deltaTime) {
             base.Update(world, deltaTime);
