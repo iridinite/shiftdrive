39bd0dcbbac6e4d6c823efa9d5fa8963bdc5b19a
iridinite <codingcuddlewolf@gmail.com>
2016-07-29 19:57:39 +0200

    Add AI task enum

---
+++ Client/AITask.cs
@@ -0,0 +1,22 @@
+﻿/*
+** Project ShiftDrive
+** (C) Mika Molenkamp, 2016.
+*/
+
+namespace ShiftDrive {
+
+    /// <summary>
+    /// Represents a task for an AI-controlled ship
+    /// </summary>
+    internal enum AITask {
+        Invalid = 0,
+        TravelDestination = 1,
+        TravelRandomize = 2,
+        ChaseEnemy = 3,
+        ChaseStation = 4,
+        ChaseAnger = 5,
+        AvoidMine = 6,
+        AvoidBlackHole = 7
+    }
+
+}
