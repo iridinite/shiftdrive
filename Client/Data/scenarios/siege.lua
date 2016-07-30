009943c7d5401befcc7bca7f974171c97d1170c6
iridinite <codingcuddlewolf@gmail.com>
2016-07-31 00:58:26 +0200

    Change test area again
    Disabled the creation of npcs and black holes, until I add sprite sheets
    for those

--- Client/Data/scenarios/siege.lua
+++ Client/Data/scenarios/siege.lua
@@ -5,11 +5,11 @@ local player = CreateShip(require("ships/player-lcruiser"), "player")
 player.position = {400, 500}
 player.fuel = 5.4
 
-local npc = CreateShip(require("ships/bulkcargo"))
+--[[local npc = CreateShip(require("ships/bulkcargo"))
 npc.position = {400, 450}
-
+]]
 Create("asteroid", {startpoint = {0, 0}, endpoint = {1000, 1000}, range = 200, count = 300})
-
+--[[
 for i = 1, 5 do
   local bh = Create("blackhole")
   repeat
@@ -17,3 +17,4 @@ for i = 1, 5 do
     bh.position = {math.random(0, 1000), math.random(0, 1000)}
   until math.dist(player.position, bh.position) > 200
 end
+]]
