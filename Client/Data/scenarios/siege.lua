5eff4dd1ca6368edd8d1d7781ef39cb799921b72
iridinite <codingcuddlewolf@gmail.com>
2016-08-05 22:05:17 +0200

    Update map script
    Refer to new broadside file, did cleanup.

--- Client/Data/scenarios/siege.lua
+++ Client/Data/scenarios/siege.lua
@@ -1,20 +1,6 @@
--- Siege gamemode
--- creates a fairly generic world with linear progression
+local player = CreateShip(require("ships/broadside"), "player")
+player.position = {500, 500}
+player.fuel = 11
 
-local player = CreateShip(require("ships/player-lcruiser"), "player")
-player.position = {400, 500}
-player.fuel = 5.4
-
---[[local npc = CreateShip(require("ships/bulkcargo"))
-npc.position = {400, 450}
-]]
-Create("asteroid", {startpoint = {0, 0}, endpoint = {1000, 1000}, range = 200, count = 300})
---[[
-for i = 1, 5 do
-  local bh = Create("blackhole")
-  repeat
-    -- require black hole position to be a minimum safe distance away
-    bh.position = {math.random(0, 1000), math.random(0, 1000)}
-  until math.dist(player.position, bh.position) > 200
-end
-]]
+Create("asteroid", {startpoint = {0, 0}, endpoint = {1000, 1000}, range = 100, count = 100})
+Create("blackhole").position = {350, 400}
