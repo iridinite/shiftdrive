11205dd54f978252798f4c67a516997d74944d7b
iridinite <codingcuddlewolf@gmail.com>
2016-08-05 22:06:18 +0200

    Implement hull bar decline delay
    The purple area of the hull bar that shows how much hull integrity was
    removed now waits around for just under a second before it begins
    scrolling to meet the new hull value.

--- Client/FormGame.cs
+++ Client/FormGame.cs
@@ -15,10 +15,14 @@ namespace ShiftDrive {
         private readonly List<Button> consoleButtons;
         private float hullFlicker;
         private float hullPrevious;
+        private float hullDeclineWait;
+        private float hullDecline;
 
         public FormGame() {
             hullFlicker = 0f;
             hullPrevious = 0f;
+            hullDeclineWait = 0f;
+            hullDecline = 0f;
 
             consoleButtons = new List<Button>();
             Button btnHelm = new Button(0, 3, 3, 100, 35, "HELM");
@@ -64,7 +68,7 @@ namespace ShiftDrive {
                 Color outlineColor = hullFraction <= 0.35f && hullFlicker >= 0.5f ? Color.Red : Color.White;
                 Color hullbarColor = hullFraction <= 0.35f ? Color.Red : hullFraction <= 0.7f ? Color.Orange : Color.Green;
                 spriteBatch.Draw(Assets.textures["ui/hullbar"], new Rectangle(hullbarx, 0, 512, 64), new Rectangle(0, 64, 512, 64), Color.White);
-                spriteBatch.Draw(Assets.textures["ui/hullbar"], new Rectangle(hullbarx, 0, (int)(hullPrevious / player.hullMax * 512f), 64), new Rectangle(0, 128, (int)(hullPrevious / player.hullMax * 512f), 64), Color.DarkRed);
+                spriteBatch.Draw(Assets.textures["ui/hullbar"], new Rectangle(hullbarx, 0, (int)(hullDecline / player.hullMax * 512f), 64), new Rectangle(0, 128, (int)(hullDecline / player.hullMax * 512f), 64), Color.Purple);
                 spriteBatch.Draw(Assets.textures["ui/hullbar"], new Rectangle(hullbarx, 0, (int)(hullFraction * 512f), 64), new Rectangle(0, 128, (int)(hullFraction * 512f), 64), hullbarColor);
                 spriteBatch.Draw(Assets.textures["ui/hullbar"], new Rectangle(hullbarx, 0, 512, 64), new Rectangle(0, 0, 512, 64), outlineColor);
                 spriteBatch.DrawString(Assets.fontDefault, (int)(hullFraction * 100f) + "%", new Vector2(hullbarx + 472, 34), Color.Black);
@@ -78,21 +82,33 @@ namespace ShiftDrive {
         }
 
         public void Update(GameTime gameTime) {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
             lock (NetClient.worldLock) {
                 Console.Update(gameTime);
 
                 // show and animate negative changes to hull integrity.
                 PlayerShip player = NetClient.World.GetPlayerShip();
-                if (player.hull > hullPrevious) // no animation for upped hull
+                if (player.hull > hullDecline) { // no animation for upped hull
+                    hullDecline = player.hull;
                     hullPrevious = player.hull;
-                if (hullPrevious > player.hull) // player just took damage
-                    hullPrevious = MathHelper.Max(player.hull, hullPrevious - (float)(gameTime.ElapsedGameTime.TotalSeconds * player.hullMax * 0.2f));
+                }
+                if (hullDeclineWait > 0f)
+                    hullDeclineWait -= dt;
+                if (hullDecline > player.hull) { // there is still a decline bar to show
+                    if (hullPrevious > player.hull) { // if we just took damage, wait before declining
+                        hullDeclineWait = 0.75f;
+                        hullPrevious = player.hull;
+                    }
+                    if (hullDeclineWait <= 0f) // animate the hull bar declination
+                        hullDecline = MathHelper.Max(player.hull, hullDecline - dt * player.hullMax * 0.2f);
+                }
+
 
                 foreach (Button b in consoleButtons)
                     b.Update(gameTime);
             }
 
-            hullFlicker += (float) gameTime.ElapsedGameTime.TotalSeconds;
+            hullFlicker += dt;
             if (hullFlicker >= 1f) hullFlicker = 0f;
         }
     }
