9297dc978102c9f141a32f237a0f0e6c875f7efb
iridinite <codingcuddlewolf@gmail.com>
2016-08-07 23:37:39 +0200

    Replace player ship with mdfrigate

--- Client/Data/scenarios/siege.lua
+++ Client/Data/scenarios/siege.lua
@@ -1,6 +1,10 @@
-local player = CreateShip(require("ships/broadside"), "player")
+Create("blackhole").position = {350, 400}
+
+local player = CreateShip(require("ships/mdfrigate"), "player")
 player.position = {500, 500}
 player.fuel = 11
 
-Create("asteroid", {startpoint = {0, 0}, endpoint = {1000, 1000}, range = 100, count = 100})
-Create("blackhole").position = {350, 400}
+local npc = CreateShip(require("ships/broadside"))
+npc.position = {600, 500}
+
+--Create("asteroid", {startpoint = {0, 0}, endpoint = {1000, 1000}, range = 100, count = 100})
