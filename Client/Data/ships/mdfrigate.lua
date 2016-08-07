fac7b1aa13d686952644a407222a246879983798
iridinite <codingcuddlewolf@gmail.com>
2016-08-07 23:33:39 +0200

    Add mdfrigate Lua script

---
+++ Client/Data/ships/mdfrigate.lua
@@ -0,0 +1,39 @@
+return {
+  nameshort = GetRandomShipId(),
+  namefull = localize("ship_mdfrigate"),
+  
+  topspeed = 30,
+  turnrate = 30,
+  hullmax = 100,
+  shieldmax = 100,
+  cargo = 2,
+  bounding = 20,
+  
+  sprite = "ship/mdfrigate",
+  
+  mounts = {
+    {
+      position = {0, 0},
+      bearing = 0,
+      arc = 100,
+      size = Mount.SMALL
+    },
+    {
+      position = {10, 4},
+      bearing = 0,
+      arc = 100,
+      size = Mount.SMALL
+    },
+    {
+      position = {-10, 4},
+      bearing = 0,
+      arc = 100,
+      size = Mount.SMALL
+    }
+  },
+  weapons = {
+    require("weapons/burstlaser"),
+    require("weapons/autocannon"),
+    require("weapons/autocannon")
+  }
+}
