e45132b62227078152ca62e3d61d4f71efdb6548
iridinite <codingcuddlewolf@gmail.com>
2016-08-17 17:33:05 +0200

    Track and apply Projectile damage

--- Client/Projectile.cs
+++ Client/Projectile.cs
@@ -13,8 +13,9 @@ namespace ShiftDrive {
     /// Represents a projectile in flight.
     /// </summary>
     internal sealed class Projectile : GameObject {
-        
+
         public float lifetime;
+        public float damage;
         public byte faction;
 
         public Projectile(GameState world) : base(world) {
@@ -25,15 +26,16 @@ namespace ShiftDrive {
             layermask = CollisionLayer.Ship | CollisionLayer.Asteroid;
         }
 
-        public Projectile(GameState world, string spritename, Vector2 position, float facing, float speed, byte faction) : this(world) {
+        public Projectile(GameState world, string spritename, Vector2 position, float facing, float speed, float damage,
+            byte faction) : this(world) {
             this.spritename = spritename;
-            //this.sprite = Assets.GetTexture(spritename);
             this.position = position;
             this.facing = facing;
             this.velocity = speed * new Vector2(
                 (float)Math.Cos(MathHelper.ToRadians(facing - 90f)),
                 (float)Math.Sin(MathHelper.ToRadians(facing - 90f)));
             this.faction = faction;
+            this.damage = damage;
             this.lifetime = 0f;
         }
 
@@ -43,8 +45,6 @@ namespace ShiftDrive {
             // move forward
             lifetime += deltaTime;
 
-            // TODO: collision detection
-
             // get rid of projectiles that have traveled very far already
             if (world.IsServer && lifetime >= 10f) Destroy();
 
@@ -52,9 +52,15 @@ namespace ShiftDrive {
             changed = true;
         }
 
-        public override void Destroy() {
-            // detonate
-            base.Destroy();
+        protected override void OnCollision(GameObject other, Vector2 normal, float penetration) {
+            // ignore hits on friendly ships
+            if (other.IsShip()) {
+                Ship othership = other as Ship;
+                if (othership?.faction == this.faction) return;
+            }
+            // apply damage to whatever we hit
+            other.TakeDamage(damage);
+            this.Destroy();
         }
         
     }
