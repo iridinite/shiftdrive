47d9b51bcba5d19bf21c009e4b93ea5333066346
iridinite <codingcuddlewolf@gmail.com>
2016-08-17 21:44:17 +0200

    Set ship factions to allow damage

--- Client/Data/scenarios/siege.lua
+++ Client/Data/scenarios/siege.lua
@@ -3,8 +3,10 @@ Create("blackhole").position = {350, 400}
 local player = CreateShip(require("ships/mdfrigate"), "player")
 player.position = {500, 500}
 player.fuel = 11
+player.faction = 1
 
 local npc = CreateShip(require("ships/broadside"))
 npc.position = {600, 500}
+npc.faction = 2
 
---Create("asteroid", {startpoint = {0, 0}, endpoint = {1000, 1000}, range = 100, count = 100})
+Create("asteroid", {startpoint = {0, 0}, endpoint = {1000, 1000}, range = 100, count = 50})
