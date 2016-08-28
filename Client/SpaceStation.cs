cbda438b7907cb8f0b6fbe74d4e74692963271dd
iridinite <codingcuddlewolf@gmail.com>
2016-08-28 17:41:46 +0200

    Add SpaceStation class

---
+++ Client/SpaceStation.cs
@@ -0,0 +1,31 @@
+﻿/*
+** Project ShiftDrive
+** (C) Mika Molenkamp, 2016.
+*/
+
+using Microsoft.Xna.Framework;
+
+namespace ShiftDrive {
+
+    internal sealed class SpaceStation : Ship {
+
+        public SpaceStation(GameState world) : base(world) {
+            type = ObjectType.Station;
+            bounding = 100f;
+        }
+
+        protected override void OnCollision(GameObject other, Vector2 normal, float penetration) {
+            // no action; station doesn't care about collision
+        }
+
+        public override void Update(float deltaTime) {
+            // station never moves
+            throttle = 0f;
+            facing = 0f;
+            steering = 0f;
+            // base update
+            base.Update(deltaTime);
+        }
+    }
+
+}
