4952f81e36be082c55e8916b44fba5b623db010d
iridinite <codingcuddlewolf@gmail.com>
2016-08-28 23:22:15 +0200

    Overhaul weapon block display

--- Client/ConsoleWeapons.cs
+++ Client/ConsoleWeapons.cs
@@ -20,24 +20,25 @@ namespace ShiftDrive {
             DrawFuelGauge(spriteBatch);
 
             // draw a list of currently active weapons
-            int weaponBoxY = SDGame.Inst.GameHeight - Player.mountsNum * 90 - 20;
+            int weaponBoxX = SDGame.Inst.GameWidth / 2 - Player.mountsNum * 80;
             for (int i = 0; i < Player.mountsNum; i++) {
                 // draw box background
-                int weaponCurrentY = weaponBoxY + i * 90;
-                spriteBatch.Draw(Assets.textures["ui/rect"], new Rectangle(0, weaponCurrentY, 230, 80), Color.Gray);
+                int weaponCurrentX = weaponBoxX + i * 160;
+                spriteBatch.Draw(Assets.textures["ui/rect"], new Rectangle(weaponCurrentX, SDGame.Inst.GameHeight - 60, 150, 60), Color.Gray);
 
                 // find the weapon in this index
                 Weapon wep = Player.weapons[i];
                 if (wep == null) {
-                    spriteBatch.DrawString(Assets.fontDefault, Utils.LocaleGet("no_weapon"), new Vector2(18, weaponCurrentY + 10), Color.FromNonPremultiplied(48, 48, 48, 255));
+                    spriteBatch.DrawString(Assets.fontDefault, Utils.LocaleGet("no_weapon"), new Vector2(weaponCurrentX + 8, SDGame.Inst.GameHeight - 52), Color.FromNonPremultiplied(48, 48, 48, 255));
                     continue;
                 }
-                
+
                 // draw weapon details
-                spriteBatch.Draw(Assets.textures["ui/itemicons"], new Rectangle(188, weaponCurrentY + 10, 32, 32), new Rectangle((int)wep.DamageType * 32, 32, 32, 32), Color.White);
-                spriteBatch.Draw(Assets.textures["ui/chargebar"], new Rectangle(16, weaponCurrentY + 55, 128, 16), new Rectangle(0, 16, 128, 16), Color.White);
-                spriteBatch.DrawString(Assets.fontDefault, wep.Name, new Vector2(18, weaponCurrentY + 10), Color.Black);
-                spriteBatch.DrawString(Assets.fontDefault, wep.Name, new Vector2(16, weaponCurrentY + 8), Color.White);
+                int chargeWidth = (int)(128 * (wep.Charge / wep.ChargeTime));
+                spriteBatch.Draw(Assets.textures["ui/chargebar"], new Rectangle(weaponCurrentX + 11, SDGame.Inst.GameHeight - 32, chargeWidth, 16), new Rectangle(0, 0, chargeWidth, 16), Color.LightGoldenrodYellow);
+                spriteBatch.Draw(Assets.textures["ui/chargebar"], new Rectangle(weaponCurrentX + 11, SDGame.Inst.GameHeight - 32, 128, 16), new Rectangle(0, 16, 128, 16), Color.White);
+                spriteBatch.DrawString(Assets.fontTooltip, wep.Name, new Vector2(weaponCurrentX + 9, SDGame.Inst.GameHeight - 51), Color.Black);
+                spriteBatch.DrawString(Assets.fontTooltip, wep.Name, new Vector2(weaponCurrentX + 8, SDGame.Inst.GameHeight - 52), Color.White);
             }
 
             spriteBatch.End();
