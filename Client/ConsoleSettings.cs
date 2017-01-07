8cd5c050da68580b498ede53a8a05f8eda11f2f9
iridinite <codingcuddlewolf@gmail.com>
2017-01-07 14:26:14 +0100

    Add console for in-game settings

---
+++ Client/ConsoleSettings.cs
@@ -0,0 +1,38 @@
+﻿/*
+** Project ShiftDrive
+** (C) Mika Molenkamp, 2016-2017.
+*/
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShiftDrive {
+
+    /// <summary>
+    /// Implements a <seealso cref="Console"/> showing an in-game settings menu.
+    /// </summary>
+    internal sealed class ConsoleSettings : Console {
+
+        private readonly Button btnToLobby = new TextButton(-1, -1, 300, 300, 40, Locale.Get("returntolobby"));
+
+        public ConsoleSettings() {
+            btnToLobby.OnClick += BtnToLobby_OnClick;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+            btnToLobby.Draw(spriteBatch);
+            spriteBatch.End();
+        }
+
+        public override void Update(GameTime gameTime) {
+            btnToLobby.Update(gameTime);
+        }
+
+        private void BtnToLobby_OnClick(Control sender) {
+            SDGame.Inst.ActiveForm = new FormLobby();
+        }
+
+    }
+
+}
