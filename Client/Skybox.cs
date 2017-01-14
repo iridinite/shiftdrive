10844abef970030907906e830e0677d4b75fc770
iridinite <codingcuddlewolf@gmail.com>
2017-01-14 23:33:59 +0100

    Render skybox using new shader technique

--- Client/Skybox.cs
+++ Client/Skybox.cs
@@ -15,13 +15,17 @@ namespace ShiftDrive {
 
         private static float rotation = Utils.RandomFloat(0f, MathHelper.TwoPi);
 
+        private static readonly Matrix view = Matrix.CreateLookAt(
+            new Vector3(0, 0, 0),
+            new Vector3(0f, -0.25f, 1.5f),
+            Vector3.Up);
+
         public static void Draw() {
             // Use the unlit shader to render a skybox.
-            Effect fx = Assets.fxUnlit; // shortcut
-            fx.Parameters["WVP"].SetValue(Matrix.CreateRotationY(rotation) * Matrix.CreateRotationZ(rotation) *
-                                          Matrix.CreateLookAt(new Vector3(0f, -0.25f, 1.5f), new Vector3(0, 0, 0),
-                                              Vector3.Up) * SDGame.Inst.Projection);
-            fx.Parameters["ModelTexture"].SetValue(Assets.textures["ui/skybox"]);
+            Effect fx = Assets.fxSkybox; // shortcut
+            fx.Parameters["View"].SetValue(Matrix.CreateRotationY(rotation) * Matrix.CreateRotationZ(rotation) * view);
+            fx.Parameters["Projection"].SetValue(SDGame.Inst.Projection);
+            fx.Parameters["SkyboxTexture"].SetValue(Assets.tecSkybox);
             foreach (ModelMesh mesh in Assets.mdlSkybox.Meshes) {
                 foreach (ModelMeshPart part in mesh.MeshParts) {
                     part.Effect = fx;
