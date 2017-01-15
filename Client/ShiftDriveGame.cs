261af957662b519962a4d0bf9a0553fdc69e6879
iridinite <codingcuddlewolf@gmail.com>
2017-01-15 22:28:27 +0100

    Clean up unused using directives

--- Client/ShiftDriveGame.cs
+++ Client/ShiftDriveGame.cs
@@ -4,9 +4,7 @@
 */
 
 using System;
-using System.IO;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
