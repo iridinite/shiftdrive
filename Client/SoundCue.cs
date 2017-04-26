b0435083bdac14e9aecc3f3766551f1c52020531
iridinite <codingcuddlewolf@gmail.com>
2017-04-26 15:56:04 +0200

    Add sound cooldown, falloff and positioning

--- Client/SoundCue.cs
+++ Client/SoundCue.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 
@@ -11,6 +12,7 @@ namespace ShiftDrive {
     /// </summary>
     internal sealed class SoundCue : IDisposable {
         private readonly List<SoundEffect> sounds;
+        private double lastPlayedAt;
 
         /// <summary>
         /// Gets or sets the volume, ranging from 1.0 (full volume) to 0.0 (silence).
@@ -21,6 +23,16 @@ namespace ShiftDrive {
         /// Gets or sets the pitch adjustment, ranging from 1.0 (up an octave) to 0.0 (no change) to -1.0 (down an octave).
         /// </summary>
         public float Pitch { get; set; } = 0f;
+
+        /// <summary>
+        /// Gets or sets the minimum number of seconds that must be between two instances of this sound.
+        /// </summary>
+        public float Cooldown { get; set; } = 0f;
+
+        /// <summary>
+        /// Gets or sets the volume falloff-over-distance multiplier. A value of 2 will cause volume to drop twice as fast. Default is 1.
+        /// </summary>
+        public float Falloff { get; set; } = 1f;
 
         /// <summary>
         /// Returns the number of <seealso cref="SoundEffect"/> instances loaded.
@@ -35,6 +47,7 @@ namespace ShiftDrive {
         /// </summary>
         public SoundCue() {
             sounds = new List<SoundEffect>();
+            lastPlayedAt = 0f;
         }
 
         /// <summary>
@@ -47,18 +60,42 @@ namespace ShiftDrive {
         }
 
         /// <summary>
+        /// Randomly selects a <seealso cref="SoundEffect"/> to play.
+        /// </summary>
+        private SoundEffect SelectSound() {
+            Debug.Assert(sounds.Count > 0, "SoundCue must have at least one SoundEffect");
+
+            if (Cooldown > 0f) {
+                TimeSpan totalTime = SDGame.Inst.GetTime().TotalGameTime;
+                double now = totalTime.TotalSeconds;
+                if (now < lastPlayedAt + Cooldown) return null;
+
+                lastPlayedAt = totalTime.TotalSeconds;
+            }
+
+            // select the sound to play randomly; if only one sound, avoid the expensive RNG call
+            return sounds.Count > 1
+                ? sounds[Utils.RNG.Next(sounds.Count)]
+                : sounds[0];
+        }
+
+        /// <summary>
         /// Plays this <see cref="SoundCue"/> with the currently specified settings.
         /// </summary>
         public void Play() {
-            Debug.Assert(sounds.Count > 0, "SoundCue must have at least one SoundEffect");
+            SelectSound()?.Play(Volume, Pitch, 0f);
+        }
 
-            // select the sound to play randomly; if only one sound, avoid the expensive RNG call
-            SoundEffect selected = null;
-            selected = sounds.Count > 1
-                ? sounds[Utils.RNG.Next(sounds.Count)]
-                : sounds[0];
+        /// <summary>
+        /// Plays this <see cref="SoundCue"/> in stereo, calculating panning based on object positions.
+        /// </summary>
+        public void Play3D(Vector2 listener, Vector2 emitter) {
+            Vector2 delta = emitter - listener;
+            float dist = delta.Length();
+            float instPanning = MathHelper.Clamp((emitter.X - listener.X) / 128f, -1f, 1f);
+            float instVolume = MathHelper.Clamp(1f - (dist / 200f * Falloff), 0.1f, 1f);
 
-            selected.Play(Volume, Pitch, 0f);
+            SelectSound()?.Play(instVolume * Volume, Pitch, instPanning);
         }
 
         /// <summary>
