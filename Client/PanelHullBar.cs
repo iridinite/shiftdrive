c6db10089b6bf8a7101ba459e99040961f8ee819
iridinite <codingcuddlewolf@gmail.com>
2017-06-04 22:32:31 +0200

    Tweak hull and shield bar display

--- Client/PanelHullBar.cs
+++ Client/PanelHullBar.cs
@@ -26,27 +26,37 @@ namespace ShiftDrive {
             float hullFraction = player.hull / player.hullMax;
             float shieldFraction = player.shield / player.shieldMax;
             Color outlineColor = hullFraction <= 0.35f && hullFlicker >= 0.5f ? Color.Red : Color.White;
-            Color hullbarColor = hullFraction <= 0.35f ? Color.Red : hullFraction <= 0.7f ? Color.Orange : Color.Green;
+            Color hullbarColor = hullFraction <= 0.35f ? Color.Red : hullFraction <= 0.7f ? Color.Orange : Color.SlateGray;
 
             // background
             spriteBatch.Draw(Assets.GetTexture("ui/hullbar"), new Rectangle(hullbarx, 0, 512, 64), new Rectangle(0, 64, 512, 64), Color.White);
 
-            // hull
-            spriteBatch.Draw(Assets.GetTexture("ui/hullbar"),
-                new Rectangle(hullbarx, 0, (int)(hullDecline / player.hullMax * 512f), 64),
-                new Rectangle(0, 128, (int)(hullDecline / player.hullMax * 512f), 64),
-                Color.Purple);
+            // health bars
+            if (player.shield <= 0f || !player.shieldActive) {
+                // if shields are not dead, draw decline below the main hull
+                spriteBatch.Draw(Assets.GetTexture("ui/hullbar"),
+                    new Rectangle(hullbarx, 0, (int)(hullDecline / player.hullMax * 512f), 64),
+                    new Rectangle(0, 128, (int)(hullDecline / player.hullMax * 512f), 64),
+                    Color.Purple);
+            }
+
             spriteBatch.Draw(Assets.GetTexture("ui/hullbar"),
                 new Rectangle(hullbarx, 0, (int)(hullFraction * 512f), 64),
                 new Rectangle(0, 128, (int)(hullFraction * 512f), 64),
                 hullbarColor);
 
-            // shields
-            int shieldPixels = (int)(shieldFraction * 352f); // chop off 160 pixels left
-            spriteBatch.Draw(Assets.GetTexture("ui/hullbar"),
-                new Rectangle(hullbarx + 160 + 352 - shieldPixels, 0, shieldPixels, 64),
-                new Rectangle(160 + 352 - shieldPixels, 192, shieldPixels, 64),
-                player.shieldActive ? Color.LightSkyBlue : Color.Gray);
+            if (player.shieldActive && player.shield > 0f) {
+                // if shields are active, draw decline on top of the main hull so we can still see it
+                spriteBatch.Draw(Assets.GetTexture("ui/hullbar"),
+                    new Rectangle(hullbarx, 0, (int)(hullDecline / player.shieldMax * 512f), 64),
+                    new Rectangle(0, 128, (int)(hullDecline / player.shieldMax * 512f), 64),
+                    Color.Purple);
+                spriteBatch.Draw(Assets.GetTexture("ui/hullbar"),
+                    new Rectangle(hullbarx, 0, (int)(shieldFraction * 512f), 64),
+                    new Rectangle(0, 128, (int)(shieldFraction * 512f), 64),
+                    Color.LightSkyBlue);
+            }
+
             // outline
             spriteBatch.Draw(Assets.GetTexture("ui/hullbar"), new Rectangle(hullbarx, 0, 512, 64), new Rectangle(0, 0, 512, 64), outlineColor);
         }
@@ -54,25 +64,27 @@ namespace ShiftDrive {
         protected override void OnUpdate(GameTime gameTime) {
             var dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
             var player = NetClient.World.GetPlayerShip();
+            var healthVal = player.shieldActive && player.shield > 0f ? player.shield : player.hull;
+            var healthValMax = player.shieldActive && player.shield > 0f ? player.shieldMax : player.hullMax;
 
-            if (player.hull > hullDecline) {
+            if (healthVal > hullDecline) {
                 // no animation for upped hull
-                hullDecline = player.hull;
-                hullPrevious = player.hull;
+                hullDecline = healthVal;
+                hullPrevious = healthVal;
             }
 
             if (hullDeclineWait > 0f)
                 hullDeclineWait -= dt;
 
-            if (hullDecline > player.hull) {
+            if (hullDecline > healthVal) {
                 // there is still a decline bar to show
-                if (hullPrevious > player.hull) {
+                if (hullPrevious > healthVal) {
                     // if we just took damage, wait before declining
                     hullDeclineWait = 0.75f;
-                    hullPrevious = player.hull;
+                    hullPrevious = healthVal;
                 }
                 if (hullDeclineWait <= 0f) // animate the hull integrity loss
-                    hullDecline = MathHelper.Max(player.hull, hullDecline - dt * player.hullMax * 0.2f);
+                    hullDecline = MathHelper.Max(healthVal, hullDecline - dt * healthValMax * 0.2f);
             }
 
             hullFlicker += dt;
