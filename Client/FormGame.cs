3c750c87c2d76ec93634997348e25b1a23bf4cb5
iridinite <codingcuddlewolf@gmail.com>
2017-06-04 16:46:28 +0200

    Move console buttons to separate Control

--- Client/FormGame.cs
+++ Client/FormGame.cs
@@ -3,7 +3,6 @@
 ** (C) Mika Molenkamp, 2016-2017.
 */
 
-using System.Collections.Generic;
 using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -16,47 +15,33 @@ namespace ShiftDrive {
     internal class FormGame : Control {
         private Console Console { get; set; }
 
-        private readonly List<Button> consoleButtons;
-
         private float gameOverTime;
         private float gameOverFade;
 
         public FormGame() {
+            DrawMode = ControlDrawMode.ChildrenLast;
             gameOverTime = 4f;
             gameOverFade = 0f;
 
             // create console switch buttons
-            consoleButtons = new List<Button>();
-            AddConsoleButton(0, 4, sender => Console = new ConsoleSettings(), Locale.Get("console_settings")); // settings
+            var switcher = new PanelConsoleSwitcher();
+            switcher.AddConsoleButton(0, sender => Console = new ConsoleSettings(), Locale.Get("console_settings")); // settings
+            switcher.AddConsoleButton(6, sender => Console = new ConsoleLrs(), Locale.Get("console_lrs")); // debug LRS
 
-            //
+            // add buttons for each selected role
+            Debug.Assert(NetClient.TakenRoles != 0, "no roles selected");
             if (NetClient.TakenRoles.HasFlag(PlayerRole.Helm))
-                AddConsoleButton(1, -1, sender => Console = new ConsoleHelm(), Locale.Get("console_helm"));
+                switcher.AddConsoleButton(1, sender => Console = new ConsoleHelm(), Locale.Get("console_helm"));
             if (NetClient.TakenRoles.HasFlag(PlayerRole.Weapons))
-                AddConsoleButton(2, -1, sender => Console = new ConsoleWeapons(), Locale.Get("console_wep"));
+                switcher.AddConsoleButton(2, sender => Console = new ConsoleWeapons(), Locale.Get("console_wep"));
             if (NetClient.TakenRoles.HasFlag(PlayerRole.Engineering))
-                AddConsoleButton(3, -1, null, Locale.Get("console_eng"));
+                switcher.AddConsoleButton(3, null, Locale.Get("console_eng"));
             if (NetClient.TakenRoles.HasFlag(PlayerRole.Quartermaster))
-                AddConsoleButton(4, -1, null, Locale.Get("console_quar"));
+                switcher.AddConsoleButton(4, null, Locale.Get("console_quar"));
             if (NetClient.TakenRoles.HasFlag(PlayerRole.Intelligence))
-                AddConsoleButton(5, -1, null, Locale.Get("console_intel"));
+                switcher.AddConsoleButton(5, null, Locale.Get("console_intel"));
 
-            Debug.Assert(consoleButtons.Count > 1, "no Console after FormGame init");
-            AddConsoleButton(6, -1, sender => Console = new ConsoleLrs(), Locale.Get("console_lrs")); // debug LRS
-        }
-
-        private void AddConsoleButton(int icon, int y, OnClickHandler onClick, string tooltip) {
-            // unspecified y means just place at the bottom of the list
-            if (y == -1) y = consoleButtons.Count * 40 + 4;
-            // create a new button and add it to the list
-            ImageButton cbtn = new ImageButton(-1, 4, y, 36, 36, Assets.GetTexture("ui/consolebuttons"), Color.Black);
-            cbtn.SetSourceRect(new Rectangle(icon * 32, 0, 32, 32));
-            cbtn.SetTooltip(tooltip);
-            cbtn.OnClick += onClick;
-            consoleButtons.Add(cbtn);
-            // open the first ship console in the list (settings is at index 0)
-            if (consoleButtons.Count == 2)
-                onClick(cbtn);
+            Children.Add(switcher);
         }
 
         protected override void OnRender(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch) {
---
+++ Client/PanelConsoleSwitcher.cs
@@ -0,0 +1,41 @@
+﻿/*
+** Project ShiftDrive
+** (C) Mika Molenkamp, 2016-2017.
+*/
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShiftDrive {
+
+    /// <summary>
+    /// A UI element that shows a list of role buttons in the top left corner.
+    /// </summary>
+    internal sealed class PanelConsoleSwitcher : Control {
+
+        public PanelConsoleSwitcher() {
+            // our OnDraw is a background, so draw children over it
+            DrawMode = ControlDrawMode.ChildrenLast;
+        }
+
+        protected override void OnDraw(SpriteBatch spriteBatch) {
+            spriteBatch.Draw(Assets.GetTexture("ui/consolepanel"), new Rectangle(0, -496 + Children.Count * 40, 64, 512), null, Color.White,
+                0f, Vector2.Zero, SpriteEffects.None, 0f);
+        }
+
+        public void AddConsoleButton(int icon, OnClickHandler fn, string tooltip) {
+            var y = Children.Count * 40 + 4;
+            // create a new button and add it to the list
+            ImageButton cbtn = new ImageButton(-1, 4, y, 36, 36, Assets.GetTexture("ui/consolebuttons"), Color.Black);
+            cbtn.SetSourceRect(new Rectangle(icon * 32, 0, 32, 32));
+            cbtn.SetTooltip(tooltip);
+            cbtn.OnClick += fn;
+            Children.Add(cbtn);
+            // open the first ship console in the list (settings is at index 0)
+            if (Children.Count == 2)
+                fn(cbtn);
+        }
+
+    }
+
+}
