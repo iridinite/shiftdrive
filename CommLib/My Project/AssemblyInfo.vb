0150616dcc6db67ffd6344ae8d4eabb97011a844
iridinite <codingcuddlewolf@gmail.com>
2017-08-07 15:00:14 +0200

    CommLib: Use .NET 4.6, bump file version

--- CommLib/My Project/AssemblyInfo.vb
+++ CommLib/My Project/AssemblyInfo.vb
@@ -13,7 +13,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("TCP/IP Communications Library")>
 <Assembly: AssemblyCompany("Mika Molenkamp")>
 <Assembly: AssemblyProduct("")>
-<Assembly: AssemblyCopyright("Copyright © Mika Molenkamp 2013-2016")>
+<Assembly: AssemblyCopyright("Copyright © Mika Molenkamp 2013-2017")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -32,5 +32,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.1.0")> 
-<Assembly: AssemblyFileVersion("1.2.1.0")> 
+<Assembly: AssemblyVersion("1.2.2.0")> 
+<Assembly: AssemblyFileVersion("1.2.2.0")> 
